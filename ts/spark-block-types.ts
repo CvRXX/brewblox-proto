bb754a5e036a45deb520bbf607a059134836855c
Carlos van Rooijen <info@carlosvanrooijen.nl>
2022-08-02 12:11:50 +0200

    Revert "rename setpoint enabled field"
    This reverts commit d45f801daac2572ea48577386d466df4db5085bf.

--- ts/spark-block-types.ts
+++ ts/spark-block-types.ts
@@ -501,7 +501,7 @@ export interface SetpointSensorPairBlock extends Block {
   type: 'SetpointSensorPair';
   data: {
     storedSetting: Quantity;
-    enabled: boolean;
+    settingEnabled: boolean;
     filter: FilterChoice;
     filterThreshold: Quantity;
     resetFilter: boolean;
